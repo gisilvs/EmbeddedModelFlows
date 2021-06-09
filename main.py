00102fa8e7a4123f1a07b86f73f127dc638da551
GianluigiSilvestri <gianlu.silvestri@gmail.com>
2021-06-09 15:15:29 +0200

    minor changed

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from metrics import negative_elbo, forward_kl
 
 model_name = 'eight_schools'
 surrogate_posterior_name = 'normalizing_program'
-backbone_posterior_name= 'highway_flow'
+backbone_posterior_name= 'multivariate_normal'
 
 model, prior, ground_truth, target_log_prob, observations = get_model(model_name)
 surrogate_posterior = get_surrogate_posterior(prior, surrogate_posterior_name, backbone_posterior_name)
@@ -19,4 +19,5 @@ losses = tfp.vi.fit_surrogate_posterior(target_log_prob,
                                         sample_size=10)
 
 print(f'ELBO: {negative_elbo(target_log_prob, surrogate_posterior, num_smaples=15, model_name=model_name)}')
-print(f'FORWARD_KL: {forward_kl(surrogate_posterior, ground_truth)}')
+if model_name!='radon':
+  print(f'FORWARD_KL: {forward_kl(surrogate_posterior, ground_truth)}')
--- metrics.py
+++ metrics.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 
 def negative_elbo(target_log_prob, surrogate_posterior, num_smaples, model_name):
   samples = surrogate_posterior.sample(num_smaples)
-  if model_name == 'eight_schools' or model_name == 'radon':
+  if model_name == 'eight_schools':
     return - tf.reduce_mean(target_log_prob(**samples) - surrogate_posterior.log_prob(samples))
   else:
     return - tf.reduce_mean(
