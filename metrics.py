c2e13510450ff1b72972b809d3b6ba9aa4bdcf4e
GianluigiSilvestri <gianlu.silvestri@gmail.com>
2021-06-11 17:48:36 +0200

    fixed typo

--- metrics.py
+++ metrics.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 
-def negative_elbo(target_log_prob, surrogate_posterior, num_smaples, model_name):
-  samples = surrogate_posterior.sample(num_smaples, seed=45)
+def negative_elbo(target_log_prob, surrogate_posterior, num_samples, model_name, seed):
+  samples = surrogate_posterior.sample(num_samples, seed=seed)
   if model_name == 'eight_schools':
     return - tf.reduce_mean(target_log_prob(**samples) - surrogate_posterior.log_prob(samples))
   else:
