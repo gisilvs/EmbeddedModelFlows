c322854fb4a2314174eb9cb3b186550e20e3411c
GianluigiSilvestri <gianlu.silvestri@gmail.com>
2021-06-14 12:47:43 +0200

    small fixes

--- metrics.py
+++ metrics.py
@@ -2,11 +2,8 @@ import tensorflow as tf
 
 def negative_elbo(target_log_prob, surrogate_posterior, num_samples, model_name, seed):
   samples = surrogate_posterior.sample(num_samples, seed=seed)
-  if model_name == 'eight_schools':
-    return - tf.reduce_mean(target_log_prob(**samples) - surrogate_posterior.log_prob(samples))
-  else:
-    return - tf.reduce_mean(
-      target_log_prob(*samples) - surrogate_posterior.log_prob(samples))
+  return - tf.reduce_mean(
+    target_log_prob(*samples) - surrogate_posterior.log_prob(samples))
 
 def forward_kl(surrogate_posterior, ground_truth):
   return surrogate_posterior.log_prob(ground_truth)
