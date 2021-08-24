5be39c35ee040be01cee6bae5bca185b1bf5f19a
GianluigiSilvestri <gianlu.silvestri@gmail.com>
2021-08-24 17:39:42 +0200

    prototype for pixelcnn vi

--- metrics.py
+++ metrics.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 
-def negative_elbo(target_log_prob, surrogate_posterior, num_samples, model_name, seed):
+def negative_elbo(target_log_prob, surrogate_posterior, num_samples, seed):
   samples = surrogate_posterior.sample(num_samples, seed=seed)
   return - tf.reduce_mean(
     target_log_prob(*samples) - surrogate_posterior.log_prob(samples))
