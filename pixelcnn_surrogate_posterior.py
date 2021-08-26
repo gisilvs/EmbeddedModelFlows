880e7d0054a9043d690dc2f296736ff8a85f5f29
GianluigiSilvestri <gianlu.silvestri@gmail.com>
2021-08-26 10:01:49 +0200

    script loads weights of pretrained net

--- pixelcnn_surrogate_posterior.py
+++ pixelcnn_surrogate_posterior.py
@@ -2,7 +2,7 @@ import random
 
 import matplotlib.pyplot as plt
 import tensorflow as tf
-import tensorflow_datasets as tfds
+
 import tensorflow_probability as tfp
 from tensorflow_probability.python.internal import prefer_static as ps
 
@@ -85,23 +85,8 @@ def pixelcnn_as_jd(network, num_logistic_mix=5, image_side_size=28,
 
 
 image_side_size = 16
-
-# Load MNIST from tensorflow_datasets
-data = tfds.load("mnist", split=["train[:10%]", "test"])
-train_data, test_data = data[0], data[1]
-
-
-def image_preprocess(x):
-  x['image'] = tf.cast(x['image'], tf.float32)
-  x['image'] = tf.image.resize(x['image'], [image_side_size, image_side_size])
-  return (x['image'],)  # (input, output) of the model
-
-
-batch_size = 16
-train_it = train_data.map(image_preprocess).batch(batch_size).shuffle(1000)
-
 image_shape = (image_side_size, image_side_size, 1)
-# Define a Pixel CNN network
+
 dist = pixelcnn_original.PixelCNN(
   image_shape=image_shape,
   num_resnet=1,
@@ -112,22 +97,7 @@ dist = pixelcnn_original.PixelCNN(
   use_weight_norm=False,
 )
 
-# Define the model input
-image_input = tfkl.Input(shape=image_shape)
-
-# Define the log likelihood for the loss fn
-log_prob = dist.log_prob(image_input)
-
-# Define the model
-model = tfk.Model(inputs=image_input, outputs=log_prob)
-model.add_loss(-tf.reduce_mean(log_prob))
-
-# Compile and train the model
-model.compile(
-  optimizer=tfk.optimizers.Adam(.001),
-  metrics=[])
-
-model.fit(train_it, epochs=10, verbose=True)
+dist.network.load_weights(f'pixel_cnn_weights/MNIST_{image_side_size}')
 
 samples = dist.sample(5)
 seed = 10
@@ -151,8 +121,8 @@ losses = tfp.vi.fit_surrogate_posterior(target_log_prob,
                                         sample_size=5,
                                         trainable_variables=surrogate_posterior.trainable_variables)
 
-plt.plot(losses)
-plt.show()
+'''plt.plot(losses)
+plt.show()'''
 elbo = negative_elbo(target_log_prob, surrogate_posterior, num_samples=150,
                      seed=seed)
 fkl = forward_kl(surrogate_posterior, ground_truth)
