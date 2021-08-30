425f72d8dbed0908910271de074dde2ae42e099d
GianluigiSilvestri <gianlu.silvestri@gmail.com>
2021-08-30 12:46:52 +0200

    train with higher lr

--- pixelcnn_surrogate_posterior.py
+++ pixelcnn_surrogate_posterior.py
@@ -106,9 +106,9 @@ def pixelcnn_as_jd(num_logistic_mix=5, image_side_size=28,
 prior, ground_truth, target_log_prob, observations,  ground_truth_idx, observations_idx = pixelcnn_as_jd(image_side_size=image_side_size, num_observed_pixels=5,
   seed=seed)
 
-surrogate_posterior_name = 'iaf'
+surrogate_posterior_name = 'normalizing_program'
 backbone_posterior_name = 'iaf'
-num_steps = 100
+num_steps = 1000
 surrogate_posterior = get_surrogate_posterior(prior, surrogate_posterior_name,
                                               backbone_posterior_name)
 surrogate_posterior.sample()
@@ -117,7 +117,7 @@ start = time.time()
 losses = tfp.vi.fit_surrogate_posterior(target_log_prob,
                                         surrogate_posterior,
                                         optimizer=tf.keras.optimizers.Adam(
-                                        learning_rate=5e-5),
+                                        learning_rate=1e-3),
                                         # , gradient_transformers=[scale_grad_by_factor]),
                                         num_steps=num_steps,
                                         sample_size=10,
