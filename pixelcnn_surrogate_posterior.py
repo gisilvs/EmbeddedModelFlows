0a754b25eb609e4bdbff73c325196ea26442f382
GianluigiSilvestri <gianlu.silvestri@gmail.com>
2021-08-30 14:14:43 +0200

    train with lower lr

--- pixelcnn_surrogate_posterior.py
+++ pixelcnn_surrogate_posterior.py
@@ -117,7 +117,7 @@ start = time.time()
 losses = tfp.vi.fit_surrogate_posterior(target_log_prob,
                                         surrogate_posterior,
                                         optimizer=tf.keras.optimizers.Adam(
-                                        learning_rate=1e-3),
+                                        learning_rate=1e-6),
                                         # , gradient_transformers=[scale_grad_by_factor]),
                                         num_steps=num_steps,
                                         sample_size=10,
