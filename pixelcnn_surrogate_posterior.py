febcca22ed3c8c37b0dd2bf18b9bf063c235aaa5
GianluigiSilvestri <gianlu.silvestri@gmail.com>
2021-09-02 12:32:53 +0200

    restored mixture bijector

--- pixelcnn_surrogate_posterior.py
+++ pixelcnn_surrogate_posterior.py
@@ -93,8 +93,8 @@ prior, ground_truth, target_log_prob, observations,  ground_truth_idx, observati
   seed=seed)
 
 surrogate_posterior_name = 'normalizing_program'
-backbone_posterior_name = 'multivariate_normal'
-num_steps = 100
+backbone_posterior_name = 'iaf'
+num_steps = 1000
 surrogate_posterior = get_surrogate_posterior(prior, surrogate_posterior_name,
                                               backbone_posterior_name)
 surrogate_posterior.sample()
@@ -106,7 +106,7 @@ start = time.time()
 losses = tfp.vi.fit_surrogate_posterior(target_log_prob,
                                         surrogate_posterior,
                                         optimizer=tf.keras.optimizers.Adam(
-                                        learning_rate=1e-3),
+                                        learning_rate=1e-5),
                                         # , gradient_transformers=[scale_grad_by_factor]),
                                         num_steps=num_steps,
                                         sample_size=10,
