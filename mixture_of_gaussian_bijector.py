eec622f0aa986494e732ee9a8c1e6a55492e88c0
GianluigiSilvestri <gianlu.silvestri@gmail.com>
2021-09-08 12:51:31 +0200

    implemented inverse

--- mixture_of_gaussian_bijector.py
+++ mixture_of_gaussian_bijector.py
@@ -58,3 +58,57 @@ class MixtureOfGaussians(tfb.Bijector):
       _, attrs = self._augmented_inverse(y)
       cached.update(attrs)
     return cached['ildj']
+
+
+class InverseMixtureOfGaussians(tfb.Bijector):
+
+  _cache = cache_util.BijectorCacheWithGreedyAttrs(
+    forward_name='_augmented_forward', inverse_name='_augmented_inverse')
+
+  def __init__(self, dist, validate_args=False,
+               name='mixture_of_gaussians'):
+    # todo: assert dist is mixture of gaussians
+    self.dist = dist
+    super(InverseMixtureOfGaussians, self).__init__(
+      validate_args=validate_args,
+      forward_min_event_ndims=0,
+      name=name)
+
+  def forward_pass(self, x):
+    x = self.dist.cdf(x)
+    return x
+
+  def _augmented_inverse(self, y):
+    ildj = self.dist.log_prob(y)
+    return self.forward_pass(y), {'ildj': ildj, 'fldj': -ildj}
+
+  def _augmented_forward(self, x):
+    bij = tfe.bijectors.ScalarFunctionWithInferredInverse(
+      lambda e: self.forward_pass(e), max_iterations=100, root_search_fn=tfp.math.find_root_chandrupatla)
+    fldj = bij.inverse_log_det_jacobian(x)
+    x = bij.inverse(x)
+    return x, {'fldj': fldj, 'ildj': -fldj}
+
+  def _forward(self, x):
+    y, _ = self._augmented_forward(x)
+    return y
+
+  def _inverse(self, y):
+    x, _ = self._augmented_inverse(y)
+    return x
+
+  def _forward_log_det_jacobian(self, x):
+    cached = self._cache.forward_attributes(x)
+    # If LDJ isn't in the cache, call forward once.
+    if 'fldj' not in cached:
+      _, attrs = self._augmented_forward(x)
+      cached.update(attrs)
+    return cached['fldj']
+
+  def _inverse_log_det_jacobian(self, y):
+    cached = self._cache.inverse_attributes(y)
+    # If LDJ isn't in the cache, call inverse once.
+    if 'ildj' not in cached:
+      _, attrs = self._augmented_inverse(y)
+      cached.update(attrs)
+    return cached['ildj']
