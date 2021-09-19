06cd0879a9241690d78956eccaf697157b16d938
GianluigiSilvestri <gianlu.silvestri@gmail.com>
2021-09-19 15:55:16 +0200

    new experiments

--- generative_toy_experiments.py
+++ generative_toy_experiments.py
@@ -196,7 +196,7 @@ def train(model, n_components, name, save_dir):
   print(f'{name} done!')
 
 datasets = ["8gaussians", "2spirals", 'checkerboard', "diamond"]
-models = ['sandwich']
+models = ['sandwich', 'np_maf', 'maf']
 
 main_dir = '2d_toy_results'
 if not os.path.isdir(main_dir):
