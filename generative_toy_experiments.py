3c55469e7a5664a39273da1b5dfe8bbadd97cc76
GianluigiSilvestri <gianlu.silvestri@gmail.com>
2021-09-20 15:20:44 +0200

    try different prefetch

--- generative_toy_experiments.py
+++ generative_toy_experiments.py
@@ -114,7 +114,7 @@ def train(model, n_components, name, save_dir):
 
   dataset = tf.data.Dataset.from_generator(functools.partial(generate_2d_data, data=data, batch_size=int(100)),
                                            output_types=tf.float32)
-  dataset = dataset.map(prior_matching_bijector).prefetch(tf.data.AUTOTUNE)
+  dataset = dataset.map(prior_matching_bijector).prefetch(1000)
   lr = 1e-4
   lr_decayed_fn = tf.keras.optimizers.schedules.CosineDecay(
     initial_learning_rate=lr, decay_steps=num_iterations)
