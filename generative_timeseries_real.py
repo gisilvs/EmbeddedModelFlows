78e50bddfb8dc638398f052a81a8e279092fc190
GianluigiSilvestri <gianlu.silvestri@gmail.com>
2021-09-30 17:25:38 +0200

    changed initialization of mul

--- generative_timeseries_real.py
+++ generative_timeseries_real.py
@@ -98,15 +98,15 @@ def train(model, name, structure, dataset_name, save_dir):
         mul = 1.
         scale = 1.
       else:
-
-        mul = tfp.util.TransformedVariable(.5, tfb.Sigmoid(low=0.1, high=0.9))
-        scale = tfp.util.TransformedVariable(1., tfb.Softplus())
+        theta = tf.Variable(0.)
+        mul = tfp.util.TransformedVariable(.1, tfb.Sigmoid(low=0.01, high=0.99))
+        scale = tfp.util.TransformedVariable(.1, tfb.Softplus())
 
 
       @tfd.JointDistributionCoroutine
       def prior_structure():
         x = yield Root(tfd.Normal(loc=0., scale=1., name='x_0'))
-        v = yield Root(tfb.Exp()(tfd.Normal(loc=0., scale=1., name='v_0')))
+        v = yield Root(tfd.Normal(loc=0., scale=1., name='v_0'))
         for t in range(1, series_len):
           x = yield tfd.Normal(loc=x, scale=v+eps, name=f'x_{t}')
           v = yield tfb.Exp()(tfd.Normal(loc=mul*(tf.math.log(v)-theta), scale=scale, name=f'v_{t}'))
@@ -171,7 +171,7 @@ def train(model, name, structure, dataset_name, save_dir):
     for x in train:
       # Optimize the model
       loss_value = optimizer_step(maf, x)
-      #print(loss_value)
+      print(loss_value)
       train_loss_avg.update_state(loss_value)
 
     train_loss_results.append(train_loss_avg.result())
