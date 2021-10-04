7b897d21e2730cff386a2c42603c4bf9cd6e790d
GianluigiSilvestri <gianlu.silvestri@gmail.com>
2021-10-04 18:42:01 +0200

    new check

--- generative_timeseries_toy.py
+++ generative_timeseries_toy.py
@@ -222,7 +222,7 @@ def train(model, name, structure, dataset_name, save_dir):
   print(f'{name} done!')
 models = ['np_maf']
 
-main_dir = 'time_series_results'
+main_dir = 'time_series_results_0'
 if not os.path.isdir(main_dir):
   os.makedirs(main_dir)
 
