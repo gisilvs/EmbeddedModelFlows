90753f47ceefda22fb5e56b90e3149d8c1b82fe3
Gianluigi Silvestri <gianluigisilvestri@192.168.0.151>
2021-11-09 19:01:08 +0100

    rerun np

--- run_experiments.py
+++ run_experiments.py
@@ -101,8 +101,8 @@ surrogate_posterior_names = [# 'mean_field',
                              #'multivariate_normal',
                              # 'asvi',
                              #'iaf',
-                             # 'normalizing_program',
-                             'gated_normalizing_program'
+                             'normalizing_program',
+                             # 'gated_normalizing_program'
 ]
 
 backbone_names = [# 'mean_field',
