925e2510f0333db2de8d1edae9f89b97fdcc9bc4
gisilvs <gianlu.silvestri@gmail.com>
2022-06-03 08:54:05 +0200

    small refactoring

--- bijectors/actnorm.py
+++ bijectors/actnorm.py
@@ -1,14 +1,14 @@
+import numpy as np
 import tensorflow as tf
 import tensorflow_probability as tfp
-import numpy as np
-
-from tensorflow_probability.python.internal import prefer_static as ps
 from tensorflow_probability.python.internal import dtype_util
+from tensorflow_probability.python.internal import prefer_static as ps
 
 tfb = tfp.bijectors
 tfd = tfp.distributions
 tfk = tf.keras
 tfkl = tfk.layers
+
 
 class ActivationNormalization(tfb.Bijector):
   """Bijector to implement Activation Normalization (ActNorm)."""
@@ -17,21 +17,21 @@ class ActivationNormalization(tfb.Bijector):
                validate_args=False, name=None):
     parameters = dict(locals())
 
-    self.is_image=is_image
+    self.is_image = is_image
     self._initialized = tf.Variable(False, trainable=False)
     self._m = tf.Variable(tf.zeros(nchan, dtype))
     self._s = tfp.util.TransformedVariable(tf.ones(nchan, dtype),
                                            tfb.Softplus())
     self._bijector = tfb.Invert(
-        tfb.Chain([
-            tfb.Scale(self._s),
-            tfb.Shift(self._m),
-        ]))
+      tfb.Chain([
+        tfb.Scale(self._s),
+        tfb.Shift(self._m),
+      ]))
     super(ActivationNormalization, self).__init__(
-        validate_args=validate_args,
-        forward_min_event_ndims=1,
-        parameters=parameters,
-        name=name or 'ActivationNormalization')
+      validate_args=validate_args,
+      forward_min_event_ndims=1,
+      parameters=parameters,
+      name=name or 'ActivationNormalization')
 
   def _inverse(self, y, **kwargs):
     with tf.control_dependencies([self._maybe_init(y, inverse=True)]):
@@ -52,6 +52,7 @@ class ActivationNormalization(tfb.Bijector):
   def _maybe_init(self, inputs, inverse):
     """Initialize if not already initialized."""
     is_image = self.is_image
+
     def _init():
       """Build the data-dependent initialization."""
       if is_image:
